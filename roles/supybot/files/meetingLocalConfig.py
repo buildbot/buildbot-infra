6a04c26024587c4ca15b7ac29622920058570f9d
Dustin J. Mitchell <dustin@mozilla.com>
2015-06-14 22:05:35 +0000

    Install and run MeetBot

---
+++ roles/supybot/files/meetingLocalConfig.py
@@ -0,0 +1,10 @@
+class Config(object):
+    logFileDir = '/home/bbuser/meetbot/'  # TODO: template
+    logUrlPrefix = 'http://supybot.buildbot.net/meetings/'
+
+    # These, you might want to change:
+    #MeetBotInfoURL = 'http://wiki.debian.org/MeetBot'
+    #filenamePattern = '%(channel)s/%%Y/%(channel)s.%%F-%%H.%%M'
+
+
+
