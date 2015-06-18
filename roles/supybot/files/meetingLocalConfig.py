612364dd71b6ed740b5ac946d5e67feaa73b0080
Dustin J. Mitchell <dustin@mozilla.com>
2015-06-18 23:28:08 +0000

    Serve meetbot minutes via HTTP

--- roles/supybot/files/meetingLocalConfig.py
+++ roles/supybot/files/meetingLocalConfig.py
@@ -1,6 +1,6 @@
 class Config(object):
-    logFileDir = '/home/bbuser/meetbot/'  # TODO: template
-    logUrlPrefix = 'http://supybot.buildbot.net/meetings/'
+    logFileDir = '/home/bbuser/web/meetings/'
+    logUrlPrefix = 'https://supybot.buildbot.net/meetings/'
 
     # These, you might want to change:
     #MeetBotInfoURL = 'http://wiki.debian.org/MeetBot'
