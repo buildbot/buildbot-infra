163337f3c1c81720deb632beb08f073f2d53b08f
Dustin J. Mitchell <dustin@mozilla.com>
2016-01-01 19:54:16 +0000

    send mergeable PR email from the trac jail, too

---
+++ files/mergeable.py
@@ -0,0 +1,24 @@
+import requests
+import smtplib
+
+FROM = 'dustin@buildbot.net'
+RECIPIENT = ['devel@buildbot.net']
+
+def main():
+    r = requests.get('https://api.github.com/repos/buildbot/buildbot/issues?labels=merge me')
+    body = [ "%(html_url)s - %(title)s" % pr for pr in r.json() ]
+    if not body:
+        return
+
+    body = "Mergeable pull requests:\n\n" + "\n".join(body)
+
+    smtp = smtplib.SMTP('localhost')
+    smtp.sendmail(FROM, RECIPIENT, """\
+Subject: Mergeable Buildbot pull requests
+To: %s
+
+%s""" % (RECIPIENT, body))
+
+if __name__ == "__main__":
+    main()
+
