0725506f4aa0690dd791cac4468eaca533d2dfa8
Povilas Kanapickas <povilas@radix.lt>
2024-05-04 19:22:58 +0300

    Remove no longer used supybot jail

--- roles/supybot/files/meetingLocalConfig.py
+++
@@ -1,7 +0,0 @@
-class Config(object):
-    logFileDir = '/home/bbuser/web/meetings/'
-    logUrlPrefix = 'https://supybot.buildbot.net/meetings/'
-
-    # These, you might want to change:
-    # MeetBotInfoURL = 'http://wiki.debian.org/MeetBot'
-    # filenamePattern = '%(channel)s/%%Y/%(channel)s.%%F-%%H.%%M'
